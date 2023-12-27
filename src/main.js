08a03ce91271b99a7c74dcd6279d82f915b772c3
maaislam <maaislam@outlook.com>
2023-12-28 02:12:00 +0600

    folder restructure

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import './sass/styles.scss';
 
-import { obsIntersection } from './src/helpers/utils';
-import navIntersecHelper from './src/handlers/navIntersectionHelper';
-import formSubmitHandler from './src/handlers/formSumbithandler';
+import { obsIntersection } from './helpers/utils';
+import navIntersecHelper from './handlers/navIntersectionHelper';
+import formSubmitHandler from './handlers/formSumbithandler';
 
 // constants values
 const NAV_INTERSECTION_THRESHOLD = 0.2;
